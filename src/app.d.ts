5095f3684d65e3684ed606162e5b728602237821
Patryk Pozniak <ppozniak95@gmail.com>
2023-02-04 20:38:58 +0000

    Use SVGs for logo. Add inline imports

--- src/app.d.ts
+++ src/app.d.ts
@@ -17,6 +17,30 @@ declare global {
 		const out;
 		export default out;
 	}
+
+	declare module '*.svg?component' {
+		import type { ComponentType, SvelteComponentTyped } from 'svelte';
+		import type { SVGAttributes } from 'svelte/elements';
+
+		const content: ComponentType<SvelteComponentTyped<SVGAttributes<SVGSVGElement>>>;
+
+		export default content;
+	}
+
+	declare module '*.svg?src' {
+		const content: string;
+		export default content;
+	}
+
+	declare module '*.svg?url' {
+		const content: string;
+		export default content;
+	}
+
+	declare module '*.svg?dataurl' {
+		const content: string;
+		export default content;
+	}
 }
 
 export {};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,47 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
+import svg from '@poppanator/sveltekit-svg';
 
 const config: UserConfig = {
-	plugins: [sveltekit(), imagetools()]
+	plugins: [
+		sveltekit(),
+		imagetools(),
+		svg({
+			includePaths: ['./src/lib/icons/'],
+			svgoOptions: {
+				multipass: true,
+				plugins: [
+					{
+						name: 'preset-default',
+						// by default svgo removes the viewBox which prevents svg icons from scaling
+						// not a good idea! https://github.com/svg/svgo/pull/1461
+						params: {
+							overrides: {
+								removeViewBox: false
+							}
+						}
+					},
+					{ name: 'removeAttrs', params: { attrs: '(fill|stroke)' } },
+					'removeDimensions'
+				]
+			}
+		}),
+		svg({
+			includePaths: ['./src/lib/images'],
+			svgoOptions: {
+				multipass: true,
+				plugins: [
+					{
+						name: 'preset-default',
+						// by default svgo removes the viewBox which prevents svg icons from scaling
+						// not a good idea! https://github.com/svg/svgo/pull/1461
+						params: { overrides: { removeViewBox: false, removeTitle: false } }
+					}
+				]
+			}
+		})
+	]
 	// @TODO: Why is this erroring?
 	// test: {
 	// 	include: ['src/**/*.{test,spec}.{js,ts}']
