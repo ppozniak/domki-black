3f9ab0c7b5ed06af06faa57c4bed7a880a974707
Patryk Pozniak <ppozniak95@gmail.com>
2023-02-04 15:34:25 +0000

    Update hero section

--- src/partials/hero.svelte
+++ src/partials/hero.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	export let title: string = 'Twoje miejsce blisko morza.';
 
-	import heroImgMobile from '$lib/images/hero--mobile.jpg?webp&quality=90&imagetools';
-	import heroImgDesktop from '$lib/images/hero--desktop.jpg?webp&quality=90&imagetools';
+	import heroImgMobile from '$lib/images/hero--mobile.jpg?webp&quality=90&blur=3&imagetools';
+	import heroImgDesktop from '$lib/images/hero--desktop.jpg?webp&quality=90&blur=3&imagetools';
 </script>
 
-<section class="section hero" style="--image-mobile: url({heroImgMobile}); --image-desktop: url({heroImgDesktop});">
-	<div class="container">
-		<h2 class="hero__title">{title}</h2>
-	</div>
+<section
+	class="section hero"
+	style="--image-mobile: url({heroImgMobile}); --image-desktop: url({heroImgDesktop});"
+>
+	<header class="container hero__header">
+		<h2 class="hero__title">Domki letniskowie w&nbsp;Sarbinowie</h2>
+		<p class="hero__sub-title">{title}</p>
+	</header>
 
 	<div class="features">
 		<div class="container">
@@ -66,18 +70,25 @@
 					>
 				</li>
 			</ul>
-			<a class="button hero-cta" href="#kontakt">Zapytaj o dostępność</a>
 		</div>
 	</div>
+
+	<footer class="hero-cta-container container">
+		<a class="button button--inverted" href="#kontakt">Zapytaj o dostępność</a>
+		<a class="button" href="#cennik">Zobacz cennik</a>
+	</footer>
 </section>
 
 <style lang="scss">
-	@use "sass:math";
+	@use 'sass:math';
 	@import '../styles/utils';
 
 	.section.hero {
-		$overlay-min-opacity: 0.1;
-		$overlay-max-opacity: 0.25;
+		$overlay-min-opacity: 0.35;
+		$overlay-max-opacity: 0.8;
+		$overlay-color: $black;
+		$background-color: lighten($black, 50%);
+
 		scroll-margin-top: 0 !important;
 		scroll-margin: 0 !important;
 		scroll-padding-top: 0 !important;
@@ -86,23 +97,27 @@
 		min-height: 95vh;
 		background: linear-gradient(
 				to bottom,
-				rgba(#fff, $overlay-min-opacity),
-				rgba(#fff, $overlay-max-opacity)
+				rgba($overlay-color, $overlay-min-opacity),
+				rgba($overlay-color, $overlay-max-opacity)
 			),
-			var(--image-mobile) no-repeat top center/cover fixed;
+			var(--image-mobile) no-repeat top center/cover fixed,
+			$background-color;
 		color: #fff;
 		box-sizing: border-box;
-		text-align: center;
-		@include from-portrait {
+
+		@include from-landscape {
 			min-height: 100vh;
 			padding-top: $vflow * 4;
 		}
+
 		@include from-landscape {
-			background-image: linear-gradient(
+			background: linear-gradient(
 					to bottom,
-					rgba(#fff, $overlay-min-opacity),
-					rgba(#fff, $overlay-max-opacity)
+					rgba($overlay-color, $overlay-min-opacity),
+					rgba($overlay-color, $overlay-max-opacity)
 				),
+				var(--image-desktop) no-repeat top center/cover fixed,
+				$background-color;
 		}
 
 		// Fix for iOS background cover + fixed being blurred
@@ -112,30 +127,52 @@
 	}
 
 	.hero__title {
-		font-family: 'Cormorant Garamond', serif;
-		font-size: 3.5rem;
+		text-align: left;
+		font-family: inherit;
+		font-size: 2.8rem;
 		font-weight: normal;
 		line-height: 1.3;
 		color: white;
-		text-shadow: -2px 2px 4px black;
+		text-shadow: 1px 1px 3px $black;
+		letter-spacing: 1px;
+		margin: 0;
+		margin-bottom: $vflow;
+
 		@include from-portrait {
-			font-size: 4rem;
+			font-size: 3rem;
+		}
+	}
+
+	.hero__sub-title {
+		font-family: 'Cormorant Garamond', serif;
+		font-size: 1.8rem;
+		font-weight: normal;
+		line-height: 1.3;
+		color: white;
+		text-shadow: 1px 1px 3px $black;
+		margin: 0;
+		margin-bottom: $vflow;
+
+		@include from-portrait {
+			font-size: 2rem;
 		}
 	}
 
 	.features {
-		flex-grow: 1;
-		padding: $vflow;
-		@include from-landscape {
-		}
+		flex-grow: 0;
+		padding: 1rem 0;
 	}
 
 	.features__list {
 		@extend %list-reset;
 		display: flex;
 		flex-wrap: wrap;
-		justify-content: center;
+		justify-content: space-between;
 		align-items: flex-start;
+
+		@include from-landscape {
+			justify-content: center;
+		}
 	}
 
 	.feature {
@@ -143,8 +180,7 @@
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		opacity: 0.95;
-		padding: 0 0.1rem 1.5rem;
+		padding: 0 0.1rem 1rem;
 		flex: 1 33.333%;
 	}
 
@@ -153,13 +189,13 @@
 		display: flex;
 		width: $icon-size;
 		height: $icon-size;
-		border: 2px solid $black;
-		padding: 0.7rem;
+		border: 2px solid rgba(0, 0, 0, 0.7);
+		padding: 0.85rem;
 		border-radius: 50%;
 		margin-bottom: Rem(8px);
 		justify-content: center;
 		align-items: center;
-		background-color: rgba(#fff, 0.85);
+		background-color: rgba(#fff, 0.8);
 	}
 
 	.feature__icon {
@@ -172,10 +208,33 @@
 		text-transform: uppercase;
 		font-size: Rem(15px);
 		color: white;
-		text-shadow: -2px 2px 4px black;
+		text-shadow: 1px 1px 2px $black;
+		letter-spacing: 0.4px;
+		font-weight: normal;
+		padding: 0 0.2rem;
 	}
 
-	.hero-cta {
+	.hero-cta-container {
+		display: flex;
+		flex-direction: column;
 		margin-top: $vflow;
+		align-items: center;
+		justify-content: center;
+
+		> * {
+			width: 100%;
+		}
+
+		* + * {
+			margin-top: Rem(18px);
+			@include from-landscape {
+				margin-top: 0;
+				margin-left: Rem(18px);
+			}
+		}
+
+		@include from-landscape {
+			flex-direction: row;
+		}
 	}
 </style>
