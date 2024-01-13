dfd202c91d4ea6074f3814f208b3c79cdab93bdf
Patryk Pozniak <ppozniak95@gmail.com>
2024-01-13 18:19:32 +0000

    Fetch price table from Contentful

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,33 @@
 import type { PageServerLoad } from './$types';
+import contentful from 'contentful';
+import {
+	CONTENTFUL_SPACE_ID,
+	CONTENTFUL_ACCESS_TOKEN,
+	CONTENTFUL_PRICE_TABLE_ENTRY_ID
+} from '$env/static/private';
 
-export const load = (async ({ fetch }) => {
-	const res = await fetch(`/pricing.json`);
-	const prices = await res.json();
-	return prices;
+type ContentfulPriceTable = {
+	contentTypeId: 'priceTable';
+	fields: {
+		rows: contentful.EntryFieldTypes.Object<{
+			primary: Record<string, number>;
+			secondary: Record<string, number>;
+		}>;
+	};
+};
+
+export const load = (async () => {
+	// Get price table from contentful
+	const client = contentful.createClient({
+		space: CONTENTFUL_SPACE_ID,
+		accessToken: CONTENTFUL_ACCESS_TOKEN
+	});
+
+	const priceTableData = await client.getEntry<ContentfulPriceTable>(
+		CONTENTFUL_PRICE_TABLE_ENTRY_ID
+	);
+
+	return priceTableData.fields.rows;
 }) satisfies PageServerLoad;
-
 
 export const prerender = true;
